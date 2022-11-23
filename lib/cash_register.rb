717c6616a5ee3023483a2ca05902aba72892c6a7
Raman <ramanmcreates@gmail.com>
2022-11-23 14:33:03 -0500

    commit tree 7cc83efdeab461302c667f8e9a8db1b8371a6779

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
+class CashRegister
 
+    attr_reader :total
+    attr_accessor :with_discount
+
+    def initialize(with_discount = 0, total = 0)
+        @total = total
+        # @with_discount = with_discount
+    end
+
+    def total 
+        @total
+    end
+
+    def add_item(title, price, quantity = 1)
+        total += price * quantity
+    end
+
+    def apply_discount
+        total(1-with_discount) = cash_register_with_discount
+
+    end
+
+end
